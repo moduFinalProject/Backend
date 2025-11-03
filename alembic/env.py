21b73ebfa3a9022c0002cbe4ccb2e06028a36443
Ganghwai02 <dkdldb9305@gmail.com>
2025-11-03 14:18:55 +0900

    수정 사항

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,14 @@
+import os
+import sys
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from app.config import settings
+from app.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -51,16 +56,11 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        {"sqlalchemy.url": settings.DB_URL}, 
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
+        future=True
     )
 
     with connectable.connect() as connection:
