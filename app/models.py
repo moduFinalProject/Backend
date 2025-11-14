d728f82392d80901e9b824d508290b2a4dd7bc11
ganghwai <dkdldb9305@gmail.com>
2025-11-14 18:36:33 +0900

    수정

--- app/models.py
+++ app/models.py
@@ -28,7 +28,7 @@ class User(Base):
     provider = Column(VARCHAR(50))
     provider_id = Column(VARCHAR(50))
     user_type = Column(VARCHAR(10), default="1")
-    is_activate = Column(Boolean, default=True)
+    is_active = Column(Boolean, default=True)
     deleted_at = Column(TIMESTAMP(timezone=True))
     created_at = Column(
         TIMESTAMP(timezone=True), default=lambda: datetime.now(timezone.utc)
